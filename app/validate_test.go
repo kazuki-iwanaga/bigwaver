11d01b4c8b3630123d8d4bc6e8b308a6b1a66e37
kazuki-iwanaga <iwanaga@macmini.local>
2024-03-04 21:03:42 +0900

    cases := map[string] struct

--- app/validate_test.go
+++ app/validate_test.go
@@ -7,65 +7,60 @@ import (
 func TestGenerateHMAC(t *testing.T) {
 	t.Helper()
 
-	patterns := []struct {
+	cases := map[string]struct {
 		secret   string
 		message  string
 		expected string
 	}{
 		// e.g. https://docs.github.com/en/webhooks/using-webhooks/validating-webhook-deliveries#testing-the-webhook-payload-validation
-		{
-			"It's a Secret to Everybody",
-			"Hello, World!",
-			"757107ea0eb2509fc211221cce984b8a37570b6d7586c22c46f4379c8b043e17",
+		"e.g. GitHub Webhook Docs": {
+			secret:   "It's a Secret to Everybody",
+			message:  "Hello, World!",
+			expected: "757107ea0eb2509fc211221cce984b8a37570b6d7586c22c46f4379c8b043e17",
 		},
 		// e.g. https://www.php.net/manual/ja/function.hash-hmac.php
-		{
-			"secret",
-			"The quick brown fox jumped over the lazy dog.",
-			"9c5c42422b03f0ee32949920649445e417b2c634050833c5165704b825c2a53b",
+		"e.g. PHP Docs (hash-hmac)": {
+			secret:   "secret",
+			message:  "The quick brown fox jumped over the lazy dog.",
+			expected: "9c5c42422b03f0ee32949920649445e417b2c634050833c5165704b825c2a53b",
 		},
 	}
 
-	for _, p := range patterns {
-		actual := generateHMAC(p.secret, p.message)
-		if actual != p.expected {
+	for _, c := range cases {
+		actual := generateHMAC(c.secret, c.message)
+		if actual != c.expected {
 			t.Errorf(
 				"generateHMAC(%s, %s): expected %s, actual %s",
-				p.secret, p.message, p.expected, actual,
+				c.secret, c.message, c.expected, actual,
 			)
 		}
 	}
 }
 
 func TestIsSameSignature(t *testing.T) {
-	patterns := []struct {
+	cases := map[string]struct {
 		signatureA string
 		signatureB string
 		expected   bool
 	}{
-		{
-			"757107ea0eb2509fc211221cce984b8a37570b6d7586c22c46f4379c8b043e17",
-			"757107ea0eb2509fc211221cce984b8a37570b6d7586c22c46f4379c8b043e17",
-			true,
+		"same": {
+			signatureA: "757107ea0eb2509fc211221cce984b8a37570b6d7586c22c46f4379c8b043e17",
+			signatureB: "757107ea0eb2509fc211221cce984b8a37570b6d7586c22c46f4379c8b043e17",
+			expected:   true,
 		},
-		{
-			"757107ea0eb2509fc211221cce984b8a37570b6d7586c22c46f4379c8b043e17",
-			"9c5c42422b03f0ee32949920649445e417b2c634050833c5165704b825c2a53b",
-			false,
-		},
-		{
-			"9c5c42422b03f0ee32949920649445e417b2c634050833c5165704b825c2a53b",
-			"",
-			false,
+		"different": {
+			signatureA: "757107ea0eb2509fc211221cce984b8a37570b6d7586c22c46f4379c8b043e17",
+			signatureB: "9c5c42422b03f0ee32949920649445e417b2c634050833c5165704b825c2a53b",
+			expected:   false,
 		},
 	}
 
-	for _, p := range patterns {
-		actual := isSameSignature(p.signatureA, p.signatureB)
-		if actual != p.expected {
+	for _, c := range cases {
+		actual := isSameSignature(c.signatureA, c.signatureB)
+		if actual != c.expected {
 			t.Errorf(
 				"isSameSignature(%s, %s): expected %t, actual %t",
-				p.signatureA, p.signatureB, p.expected, actual,
+				c.signatureA, c.signatureB, c.expected, actual,
 			)
 		}
 	}
